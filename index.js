b299c40ffb895427832f6156f7db9584c233bea7
arthurluziano <luzianoarthur@gmail.com>
2023-12-07 17:40:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const read = require('readline-sync')
 function somaDiv() {
     var valor = Number(read.question('Digite um numero: '))
 
-    if(valor <= 0){
-        console.log('Esse número é invalido!')
-    } else {
+    if(typeof valor === 'number' && valor > 0){
         const container = []
-    
+        
         for(let x = 1; x < valor; x++){
             container.push(x)
         }
-        
+            
         var soma = 0
-    
+        
         for(x of container){
             if(x % 3 == 0 || x % 5 == 0){
                 soma += x
             }
         }
-    
+        
         console.log(`O valor inicial foi: ${valor}`)
         console.log(`A soma entre valores divisíveis por 3 ou por 5 foi: ${soma}`)
+    
+    } else {
+        console.log('O valor inserido é inválido!')
     }
 }
+
 
 somaDiv()
